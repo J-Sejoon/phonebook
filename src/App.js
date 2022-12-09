06d4dbcb2909b9265f52c91d1fd316f5e7352213
J-Sejoon <110226533+J-Sejoon@users.noreply.github.com>
2022-12-09 12:11:02 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import styles from "./App.module.css";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+
+function App() {
+	return (
+		<div className={styles.app}>
+			<h1>Phone Book</h1>
+
+			<Container>
+				<Row>
+					<Col sm='6'>
+						<ContactForm />
+					</Col>
+					<Col sm='6'>
+						<ContactList />
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+}
+
+export default App;
